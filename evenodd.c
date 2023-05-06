e88ac33a49090bfeaaf23598f902dc2db0256e52
Lakshmisinghal <132538537+Lakshmisinghal@users.noreply.github.com>
2023-05-06 15:52:19 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -2,18 +2,19 @@
 
 int main() {
     int n, i, sume = 0, sumo = 0;
-    printf("Enter the size of the array: ");
+  
     scanf("%d", &n);
     int arr[n];
-    printf("Enter the array elements:\n");
-    for (i = 0; i < n; i++) {
+   
+    for (i = 0; i < n; i++){
         scanf("%d", &arr[i]);
         if (arr[i] % 2 == 0) {
             sume += arr[i];
-        } else {
+        }
+    else {
             sumo += arr[i];
         }
     }
-    printf("The sum of the even and odd digits is %d\n", sume + sumo);
+    printf("%d", sume + sumo);
     return 0;
 }
